dc727f2e1aee9938a75e5f359c8cf14f3b764421
kerry <kerry@3eaf0391-b141-af43-b22a-34df0db0ba9f>
2009-06-16 19:50:47 +0000

    remove duplicate tests of multiply, eql?. Added test to check amount for #dollar and #franc
    git-svn-id: http://webserver:8080/svn/tdd_money@1536 3eaf0391-b141-af43-b22a-34df0db0ba9f

--- mony_spec.rb
+++ mony_spec.rb
@@ -7,24 +7,12 @@ describe Money do
     five.times(3).should eql(Money.dollar(15))
   end
 
-  it "should handle equality when using new" do
-    Money.new(10,"CHF").should eql(Franc.new(10,"CHF"))
-  end
-  
-  it "should handle equality for dollar" do
+  it "should handle equality" do
     Money.dollar(5).should eql(Money.dollar(5))
   end
 
-  it "should handle inequality for dollar" do
+  it "should handle inequality" do
     Money.dollar(5).should_not eql(Money.dollar(6))
-  end
-
-  it "should handle equality for franc" do
-    Money.franc(5).should eql(Money.franc(5))
-  end
-
-  it "should handle inequality for franc" do
-    Money.franc(5).should_not eql(Money.franc(6))
   end
 
   it "should handle equality across different currencies" do
@@ -35,8 +23,16 @@ describe Money do
     Money.franc(1).currency.should == "CHF"
   end
   
+  it "should have the right amount for franc" do
+    Money.franc(1).amount.should == 1
+  end
+  
   it "should have the right currency for dollar" do
     Money.dollar(1).currency.should == "USD"
+  end
+  
+  it "should have the right amount for dollar" do
+    Money.dollar(1).amount.should == 1
   end
   
 end
