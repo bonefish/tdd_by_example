127d38f80821a258fcbb80722b3b5cae22d9a25b
kerry <kerry@3eaf0391-b141-af43-b22a-34df0db0ba9f>
2009-06-16 21:26:26 +0000

    test fails right now, but start adding a test to convert from one currency to another
    git-svn-id: http://webserver:8080/svn/tdd_money@1547 3eaf0391-b141-af43-b22a-34df0db0ba9f

--- bank.rb
+++ bank.rb
@@ -3,5 +3,9 @@ class Bank
   def reduce(source, currency)
     source.reduce(currency)
   end
+
+  def add_rate(source, dest, rate)
     
+  end
+   
 end
--- bank_spec.rb
+++ bank_spec.rb
@@ -3,17 +3,25 @@ require 'bank'
 require 'sum'
   
 describe Bank do
+  
+  before(:each) do
+    @bank = Bank.new
+  end
 
   it "reducing a sum of the same currency gives a money on that currency" do
     sum =  Sum.new(Money.dollar(3),Money.dollar(4))
-    bank = Bank.new
-    result = bank.reduce(sum, "USD")
+    result = @bank.reduce(sum, "USD")
     result.should eql(Money.dollar(7))
   end
 
   it "should be able to reduce a single Money argument rather than a sum" do
-    bank = Bank.new
-    result = bank.reduce(Money.dollar(1), "USD")
+    result = @bank.reduce(Money.dollar(1), "USD")
+    result.should eql(Money.dollar(1))
+  end
+  
+  it "should reduce money of different currencies" do
+    @bank.add_rate("CHF", "USD", 2)
+    result = @bank.reduce(Money.franc(2), "USD")
     result.should eql(Money.dollar(1))
   end
 end
