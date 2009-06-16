1abd65fd881ff245b593be731dc26d9fc2215b72
kerry <kerry@3eaf0391-b141-af43-b22a-34df0db0ba9f>
2009-06-16 21:02:24 +0000

    write test for reduce. fails since method is just a stub
    git-svn-id: http://webserver:8080/svn/tdd_money@1542 3eaf0391-b141-af43-b22a-34df0db0ba9f

---
+++ bank_spec.rb
@@ -0,0 +1,14 @@
+require 'money'
+require 'bank'
+require 'sum'
+  
+describe Bank do
+
+  it "reducing a sum of the same currency gives a money on that currency" do
+    sum =  Sum.new(Money.dollar(3),Money.dollar(4))
+    bank = Bank.new
+    result = bank.reduce(sum, "USD")
+    result.should eql(Money.dollar(7))
+  end
+
+end
