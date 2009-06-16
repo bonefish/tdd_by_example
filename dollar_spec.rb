9f067481da459d7b23811a9b2fce539fd9401ccf
kerry <kerry@3eaf0391-b141-af43-b22a-34df0db0ba9f>
2009-06-16 18:38:17 +0000

    changed to use factory method Money.dollar
    git-svn-id: http://webserver:8080/svn/tdd_money@1520 3eaf0391-b141-af43-b22a-34df0db0ba9f

--- dollar_spec.rb
+++ dollar_spec.rb
@@ -3,19 +3,19 @@ require 'dollar'
 describe Dollar do
   it "should handle multiplication" do
     five = Money.dollar(5)
-    five.times(2).should eql(Dollar.new(10))
-    five.times(3).should eql(Dollar.new(15))
+    five.times(2).should eql(Money.dollar(10))
+    five.times(3).should eql(Money.dollar(15))
   end
 
   it "should handle equality" do
-    Dollar.new(5).should eql(Dollar.new(5))
+    Money.dollar(5).should eql(Money.dollar(5))
   end
 
   it "should handle inequality" do
-    Dollar.new(5).should_not eql(Dollar.new(6))
+    Money.dollar(5).should_not eql(Money.dollar(6))
   end
 
   it "should handle equality across different currencies" do
-    Dollar.new(5).should_not eql(Franc.new(5))
+    Money.dollar(5).should_not eql(Franc.new(5))
   end
 end
